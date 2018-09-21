93593e0dc5d1d6dfcec2c8d489370a2b6401092f
Thomas M. Maestas <thomasm1@users.noreply.github.com>
2018-09-20 22:08:26 -0600

    Create set_operations.py

---
+++ PY_DATA/pyMathData/set_operations.py
@@ -0,0 +1,14 @@
+a = {1,3,5,7}
+b = {3,4,5}
+
+union_ab = a.union(b)
+intesect_ab = a.intersection(b)
+difference_ab = a.difference(b)
+
+print('A union B = {}'.format(union.ab))
+print('A intersect B = {}'.format(intersect_ab))
+print('A difference B = {}'.format(difference_ab))
+
+A union B = {1,3,4,5,7}
+A intersect B = {3,5}
+A difference B = {1,7}
