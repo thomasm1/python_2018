1eed8f10efb09a43fe2b03c0072d3e110c04cf87
thomas maestas <thomasm1.maestas@gmail.com>
2018-09-24 08:18:26 -0600

    environment sensors finished

--- PY_IOT/environ_sense/_environ_banner.py
+++ PY_IOT/environ_sense/_environ_banner.py
@@ -10,6 +10,6 @@ sense = SenseHat()
 blue = (0,0,255)
 yellow = (255,255,0)
 red = (255, 0, 0) 
-sense_data = ('Tech Tuesday, climate_sense dashboard...' ) 
+sense_data = ('Tech Tuesday Temp Dashboard' ) 
 sense.show_message(sense_data, text_colour=yellow, back_colour=blue, scroll_speed=0.075)
  
--- PY_IOT/environ_sense/_environ_inertia.py
+++ PY_IOT/environ_sense/_environ_inertia.py
@@ -8,16 +8,19 @@ The IMU (inertial measurement unit) sensor is a combination of three sensors, ea
 '''
 #______________IMU CONFIGURATION______________set_imu_config(False, True, False)
 #________________________________(compass_enabled, gyro_enabled, accel_enabled)
-set_imu_config  #Enables and disables the gyroscope, accelerometer and/or magnetometer contribution to the get orientation functions below.
-from sense_hat import SenseHat
-sense = SenseHat()
-sense.set_imu_config(False, True, False)  # gyroscope only
+#set_imu_config  #Enables and disables the gyroscope, accelerometer and/or magnetometer contribution to the get orientation functions below.
+#from sense_hat import SenseHat
+#sense = SenseHat()
+#sense.set_imu_config(False, True, False)  # gyroscope only
 
 #______________GYROSCOPE______________get_gyroscope()
 from sense_hat import SenseHat 
 sense = SenseHat()
 gyro_only = sense.get_gyroscope()
-print("p: {pitch}, r: {roll}, y: {yaw}".format(**gyro_only)) 
+print(" ")
+print("GYROSCOPE COORDINATES, [PITCH-ROLL-YAW]")
+print(" ")
+print("pitch: {pitch}, roll: {roll}, yaw: {yaw}".format(**gyro_only)) 
 print(sense.gyro)
 print(sense.gyroscope)
 
@@ -25,6 +28,8 @@ print(sense.gyroscope)
 from sense_hat import SenseHat
 sense = SenseHat()
 accel_only = sense.get_accelerometer()
+print("ACCELEROMETER")
+print(" ")
 print("p: {pitch}, r: {roll}, y: {yaw}".format(**accel_only)) 
 print(sense.accel)
 print(sense.accelerometer)
@@ -33,6 +38,8 @@ print(sense.accelerometer)
 from sense_hat import SenseHat
 sense = SenseHat()
 orientation = sense.get_orientation()
+print(" ")
+print("MAGNETOMETER")
 print("p: {pitch}, r: {roll}, y: {yaw}".format(**orientation))
 print(sense.orientation)
 
@@ -40,5 +47,7 @@ print(sense.orientation)
 from sense_hat import SenseHat 
 sense = SenseHat()
 north = sense.get_compass()
+print(" ")
+print("Compass - bearing NORTH")
 print("North: %s" % north) 
 print(sense.compass)
