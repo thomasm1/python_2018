5404d7d145c02d34f6366abee2b489ef9ac9d992
thomas maestas <thomasm1.maestas@gmail.com>
2019-06-04 15:34:50 -0600

    enva_meetup routing adjustments and navigation

--- enva_meetup/enva_meetup/__init__.py
+++ enva_meetup/enva_meetup/__init__.py
@@ -7,24 +7,30 @@ def index():
         return render_template("index.jinja2")
         # return "hellow 2"
 
-@app.route('/hi')
-def page1():
-        return "Page 2"
+@app.route('/home')
+def home():
+        return render_template("home.jinja2")
+                #"Home"
+                
+@app.route('/home/messages')
+def messages():
+        return render_template("messages.jinja2")
 
-@app.route('/hi/<name>')
-def hi_name(name):
-        return "Hello {}".format(name)
+@app.route('/home/<input>')
+def input(input):
+        return "Input: {}".format(input)
 
-@app.route('/hi/<name>/<message>')
-def hi_name_message(name, message):
-        return render_template("hi_name_message.jinja2",
-                               name=name,
+@app.route('/home/<input>/<message>')
+def home_input_message(input, message):
+        return render_template("home_input_message.jinja2",
+                               input=input,
                                message=message)
         # return "{0}, {1}".format(name, message)
 
-@app.route('/query') # /query?a=test&b=123
+# URL: http://localhost:5000/query?a=test&b=123
+@app.route('/query') 
 def query():
         a = request.args.get('a')
         b = request.args.get('b')
-        return "a={0} b={1}".format(a,b)
-
+        return "<h4>Variable a = {0}<br />while Variable b = {1}</h4>".format(a,b)
+ 
