35b7432e065341e94c430dadad2168b75a4f3353
thomas maestas <thomasm1.maestas@gmail.com>
2019-08-14 23:58:36 -0600

    enva_django_bitcoin; models bug fix, CRUD-capable

--- enva_django_bitcoin/posts/models.py
+++ enva_django_bitcoin/posts/models.py
@@ -5,3 +5,5 @@ class Posts(models.Model):
   title = models.CharField(max_length=200)
   body = models.TextField()
   created_at = models.DateTimeField(default=datetime.now, blank=True)    
+  class Meta:
+    verbose_name_plural = "Posts"
