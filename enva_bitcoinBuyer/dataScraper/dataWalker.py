2f25666699e17cfc366c8dc3669cf894f83adf32
thomas maestas <thomasm1.maestas@gmail.com>
2019-08-20 07:01:26 -0600

    buyer: first walker

---
+++ enva_bitcoinBuyer/dataScraper/dataWalker.py
@@ -0,0 +1,34 @@
+import random
+
+def walker_one(n):
+    x = 0
+    y = 0
+    for i in range(n):
+        step = random.choice(['n','s','e','w'])
+        if step =='n':
+            y = y+1
+        elif step == 's':
+            y = y-1
+        elif step == 'e':
+            x = x+1
+        else:
+            x = x-1
+    return (x,y)
+
+for i in range(24):
+    walk = walker_one(10)
+    print(walk, "distance from home ....",
+        abs(walk[0]) + abs(walk[1]))
+
+'''
+def data_walker(n):
+    """here return coordinates is, after n walkers, where is our walker! """
+    x, y = 0, 0
+    for in range(n):
+        (dx, dy) = random.choice([0,1], (0,-1), (1,0),(-1,0)])
+        x += dx
+        y += dy
+    return (x,y)
+num_of_walkers = 100
+for walk_length in range(1,31):
+'''
