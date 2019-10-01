0c9f9eb23d8a01af1965270072f9d7fd3813f374
thomas maestas <thomasm1.maestas@gmail.com>
2019-10-01 12:01:09 -0600

    improve model; make reusable trained model

--- PY_AI/tensor_flow/tf-text-predict.py
+++ PY_AI/tensor_flow/tf-text-predict.py
@@ -4,7 +4,7 @@ import numpy as np
 
 data = keras.datasets.imdb
 
-(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
+(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=88000)
 # print(train_data[0])
  
 word_index = data.get_word_index()
@@ -28,20 +28,22 @@ def decode_review(text):
 
 # print(len(test_data[0]), len(test_data[1]))
 model = keras.Sequential()
-model.add(keras.layers.Embedding(10000, 16))
-model.add(keras.layers.GlobalAveragePooling1D())
-model.add(keras.layers.Dense(16, activation="relu"))
-model.add(keras.layers.Dense(1, activation="sigmoid"))
+model.add(keras.layers.Embedding(88000, 16))  # 1.) limit 88000 word-vectors; 2.) 16 vectors per-word embedding context
+model.add(keras.layers.GlobalAveragePooling1D()) # 3.) consolidate embedding
+model.add(keras.layers.Dense(16, activation="relu")) # 4.) rect. linear unit - functions
+model.add(keras.layers.Dense(1, activation="sigmoid")) # 5.) outputs in range (0, 1), ideal for binary class"n to find probability of data belonging to class.
 model.summary()
-model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"]) # 6.) (Unlike Softmax loss) Independent for each vector component (class)
 
-x_val = train_data[:10000]
+x_val = train_data[:10000] # x-validation
 x_train = train_data[10000:]
 
 y_val = train_labels[:10000]
 y_train = train_labels[10000:]
 
-fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
+fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1) # 512 reviews/40 versions; 
+#fitModel = model.fit(x_train, y_train, epochs=10, batch_size=512, validation_data=(x_val, y_val), verbose=1) # 512 reviews/10 versions; 
+
 results = model.evaluate(test_data, test_labels)
 print(results)  # loss .3298  accuracy: 0.8716
 
@@ -50,5 +52,10 @@ predict = model.predict([test_review])
 print("Review: ")
 print(decode_review(test_review))
 print("Actual: " + str(test_labels[0]))  # 0 
-print("Prediction: " + str(predict[0]))  # 0 bad review ... correct!!
-print(results)    # loss .33136 accuracy: 0.87112
+
+# print("Prediction: " + str(predict[0]))  # 0 bad review ... correct!!  [using 40 epochs]
+# print(results)    # loss .33136 accuracy: 0.87112 [using 40 epochs]
+print("Prediction: " + str(predict[0]))  # 0.002196 bad review ... less sure, but correct  [using 10 epochs]
+print(results)    # loss .3958 accuracy: 0.8492 [using 10 epochs] ... 0.02192 less accurate using 30 less epochs... 
+
+model.save("model.h5")
