47e1fc59e7331733cab1e489b05dfa105f0997a1
thomas maestas <thomasm1.maestas@gmail.com>
2019-09-30 22:26:19 -0600

    PY_AI: tensorflow2 model + (correct) prediction - no bugs

--- PY_AI/tensor_flow/tf-text.py
+++ PY_AI/tensor_flow/tf-text.py
@@ -6,8 +6,7 @@ data = keras.datasets.imdb
 
 (train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
 print(train_data[0])
-
-print(train_data[0])
+ 
 word_index = data.get_word_index()
 
 word_index = {k:(v+3) for k, v in word_index.items()}
@@ -18,6 +17,13 @@ word_index["<UNUSED>"] = 3
 
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
+train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], padding="post", maxlen=250)
+test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_index["<PAD>"], padding="post", maxlen=250)
+
+print(len(train_data), len(test_data))
+
 def decode_review(text):
     return " ".join([reverse_word_index.get(i, "?") for i in text])
 print(decode_review(test_data[0]))
+
+print(len(test_data[0]), len(test_data[1]))
